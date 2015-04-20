b1ab426c52964902631cd5365fb15758132f9c17
Sanjib Ahmad <sanjib@sanjib.org>
2015-04-20 15:06:24 +0600

    Create empty delegates for 3 text fields

---
+++ TextFieldsChallenge/TextFieldDelegates/CashTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  CashTextFieldDelegate.swift
+//  TextFieldsChallenge
+//
+//  Created by Sanjib Ahmad on 4/20/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
---
+++ TextFieldsChallenge/TextFieldDelegates/ZipTextFieldDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ZipUITextFieldDelegate.swift
+//  TextFieldsChallenge
+//
+//  Created by Sanjib Ahmad on 4/20/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ZipTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+}
