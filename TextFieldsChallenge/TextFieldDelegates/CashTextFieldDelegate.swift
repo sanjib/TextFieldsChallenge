e75cd43f04d33d3bedf6c8dfb1cbac324ee440d7
Sanjib Ahmad <sanjib@sanjib.org>
2015-04-20 19:17:08 +0600

    Implement delegates for cash text field

--- TextFieldsChallenge/TextFieldDelegates/CashTextFieldDelegate.swift
+++ TextFieldsChallenge/TextFieldDelegates/CashTextFieldDelegate.swift
@@ -10,5 +10,31 @@ import Foundation
 import UIKit
 
 class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.text == "" {
+            textField.text = "$0.00"
+        }
+    }
     
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        if string.toInt() != nil || range.length==1 {
+            let numberFormatter = NSNumberFormatter()
+            numberFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+            numberFormatter.locale = NSLocale(localeIdentifier: "en_US")
+            
+            var newText = textField.text as NSString
+            newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+            
+            newText = newText.stringByReplacingOccurrencesOfString("$", withString: "")
+            newText = newText.stringByReplacingOccurrencesOfString(".", withString: "")
+            newText = newText.stringByReplacingOccurrencesOfString(",", withString: "")
+            
+            var newNumber = Double(newText.doubleValue / 100)
+            textField.text = numberFormatter.stringFromNumber(newNumber)
+            
+            return false
+        }
+        return false
+    }
 }
