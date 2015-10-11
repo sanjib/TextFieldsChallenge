995434da985ba7904f81ac77ca15b14cd9c26f9d
Sanjib Ahmad <sanjib@sanjib.org>
2015-10-11 16:38:09 +0600

    fix: Update codebase to Swift 2.0

--- TextFieldsChallenge/TextFieldDelegates/CashTextFieldDelegate.swift
+++ TextFieldsChallenge/TextFieldDelegates/CashTextFieldDelegate.swift
@@ -19,19 +19,19 @@ class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
         // checking for range.length==1 to allow backspace (delete) key to work
-        if string.toInt() != nil || range.length==1 {
+        if Int(string) != nil || range.length == 1 {
             let numberFormatter = NSNumberFormatter()
             numberFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
             numberFormatter.locale = NSLocale(localeIdentifier: "en_US")
             
-            var newText = textField.text as NSString
+            var newText = textField.text! as NSString
             newText = newText.stringByReplacingCharactersInRange(range, withString: string)
             
             newText = newText.stringByReplacingOccurrencesOfString("$", withString: "")
             newText = newText.stringByReplacingOccurrencesOfString(".", withString: "")
             newText = newText.stringByReplacingOccurrencesOfString(",", withString: "")
             
-            var newNumber = Double(newText.doubleValue / 100)
+            let newNumber = Double(newText.doubleValue / 100)
             textField.text = numberFormatter.stringFromNumber(newNumber)
             
             return false
--- TextFieldsChallenge/TextFieldDelegates/ZipTextFieldDelegate.swift
+++ TextFieldsChallenge/TextFieldDelegates/ZipTextFieldDelegate.swift
@@ -14,8 +14,8 @@ class ZipTextFieldDelegate: NSObject, UITextFieldDelegate {
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
         // checking for range.length==1 to allow backspace (delete) key to work
-        if string.toInt() != nil || range.length==1 {
-            var newText = textField.text as NSString
+        if Int(string) != nil || range.length == 1 {
+            var newText = textField.text! as NSString
             newText = newText.stringByReplacingCharactersInRange(range, withString: string)
             if newText.length < 6 {
                 return true
